7162e603c4461e58fbcd764a8a4cd1ffaa61fbff
jiara <ji.arakani@gmail.com>
2022-10-16 13:16:51 -0400

    Modified sliding puzzle challenge

--- sliding-puzzle/index.js
+++ sliding-puzzle/index.js
@@ -1,3 +1,8 @@
+
+// 1. Log inputs & outputs to console
+// 2. Understand what's happening in game
+// 3. Modify challenge functions
+
 const gameGrid = [["https://i.imgur.com/zhpfEVe.png","https://i.imgur.com/u10nOUw.png","https://i.imgur.com/sEQOcGq.png"],["https://i.imgur.com/ggUqmPE.png","https://i.imgur.com/0GB90f6.png","https://i.imgur.com/nN4kAp6.png"],["https://i.imgur.com/cj9s8XS.png","https://i.imgur.com/G1kBgAO.png","https://i.imgur.com/BOkhyd6.png"]]
 
 const blankSpace = [0,gameGrid[0].length-1]
@@ -17,7 +22,6 @@ const gridWithBlank = gameGrid.map((subArray, ind) => {
       }
       return subArray.map(x => x)
     })
-
 
 const Winner = (props) => {
   return (
@@ -56,27 +60,71 @@ const Game = () => {
     
     // insert Day1 function 'checkWin' here
     // instead of return true or return false, use setIsWin(true) or setIsWin(false)
-    
+    function checkWin(a1, a2) {
+      const compare = (a, b) => {
+          return JSON.stringify(a) === JSON.stringify(b) 
+              ? true : false
+      }
+      if (a1.length !== a2.length) { setIsWin(false) }
+      for (let arr = 0; arr < a1.length; arr++) {
+          if (!compare(a1[arr], a2[arr])) { setIsWin(false) }
+      }
+      setIsWin(true)
+    }
 
     // insert Day2 move left function 'moveBlankLeft(curGrid, blankChar)' here
     // just before return, add this 'checkWin(gridWithBlank, curGrid)'
     // should return mutated array that was passed in (curGrid)
-
+function moveBlankLeft(curGrid, blankChar) {
+  const indexTarget = curGrid.indexOf(blankChar)
+  if (indexTarget > 0) {
+    const target = curGrid.splice(indexTarget, 1).pop()
+    const prev = curGrid.splice(indexTarget - 1, 0, target)
+  }
+  checkWin(gridWithBlank, curGrid)
+  return curGrid
+}
     
     // insert Day2 move right function 'moveBlankRight(curGrid, blankChar)' here
     // just before return, add this 'checkWin(gridWithBlank, curGrid)'
-    // should return mutated array that was passed in (curGrid)
-    
+    // should return mutated array that was passed in (curGrid)    
+function moveBlankRight(curGrid, blankChar) {
+  const indexTarget = curGrid.indexOf(blankChar)
+  if (indexTarget < curGrid.length - 1) {
+    const target = curGrid.splice(indexTarget, 1).pop()
+    const prev = curGrid.splice(indexTarget + 1, 0, target)
+  }
+  checkWin(gridWithBlank, curGrid)
+  return curGrid
+}
 
     // insert Day4 move up function 'moveBlankUp(curGrid, blankGrid) here'
     // just before return, add this 'checkWin(gridWithBlank, curGrid)'
     // should return mutated array that was passed in (curGrid)
-    
+function moveBlankUp(curGrid, blankGrid) {
+    const index = curGrid.indexOf(blankGrid)
+    const valParam = arr[i].splice(index, 1).pop()
+    const valPrev = arr[i - 1].splice(index, 1).pop()
+    arr[i - 1].splice(index, 0, valParam)
+    arr[i].splice(index, 0, valPrev)
+}
     
     // insert Day4 move down function 'moveBlankDown(curGrid, blankGrid) here'
     // just before return, add this 'checkWin(gridWithBlank, curGrid)'
     // should return mutated array that was passed in (curGrid)
-
+function moveBlankDown(curGrid, blankGrid) {
+  arr.forEach((a, i) => {
+      const notLastArray = i < arr.length - 1
+      const found = a.indexOf(val) >= 0
+      if (notLastArray && found) {
+        const index = a.indexOf(val)
+        const valParam = arr[i].splice(index, 1).pop()
+        const valNext = arr[i + 1].splice(index, 1).pop()
+        arr[i + 1].splice(index, 0, valParam)
+        arr[i].splice(index, 0, valNext)
+      }
+  })
+}
     
     // My found and modified Day5 shuffle function
     // Uses Durstenfeld shuffle algorithm
@@ -165,4 +213,3 @@ const Game = () => {
 }
 
 ReactDOM.render(<Game />, document.getElementById("root"))
-// 
